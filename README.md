2ae5b780d8fdc4f21cae93fdd5bb50e9a41663e8
ChristophLHR <11349102+ChristophLHR@users.noreply.github.com>
2023-04-26 14:30:32 +0200

    init

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # MiningClient-prog
+
 The Standalone Client for our bigger Mining Project
+
+This is the very simple script required to run the Client without any Server
+
+```lua
+miningClientSmall config
+```
+
+to setup the Libary in the Console.
+Very much VIP.
+
+So far only the Scanning-Radius & Mining-Diameteris are relevant
---
+++ miningClientSmall.lua
@@ -0,0 +1,79 @@
+--Requirements
+local pretty = require "cc.pretty"
+
+---@class scm
+local scm = require("scm")
+---@class SettingManager
+local sM = scm:load("settingsManager")
+---@class HelperFunctions
+local helper = scm:load("helperFunctions")
+---@class miningLib
+local miningLib = scm:load("miningClient")
+
+---@class MiningSettings
+local miningSettings = { miningDepth = -50, miningHight = 3, miningDiameter = 9, scanRadius = 4 };
+
+--Args
+args = { ... }
+
+
+--Functions
+local function facingPostition()
+    ---TODO: GPS
+    print("Facing: X|Z|-X|-Z?")
+
+    return read()
+end
+--- sets the config for the Miner
+local function config()
+    print('Settings: ')
+    print('miningDepth miningHight miningDiameter scanRadius')
+    local input = {}
+    local r = read()
+    if #r > 0 then
+        for w in r:gmatch("%S+") do
+            table.insert(input, w)
+        end
+        miningSettings = {
+            miningDepth = tonumber(input[1]),
+            miningHight = tonumber(input[2]),
+            miningDiameter = tonumber(input[3]),
+            scanRadius = tonumber(input[4])
+        };
+        miningSettings = sM.setget('MiningSettings', miningSettings, miningSettings);
+    end
+    pretty.pretty_print(miningSettings)
+end
+
+---main script Function
+local function main()
+    --TODO: GPS support
+    -- term.clear()
+
+    --settings
+    miningSettings = sM.setget('MiningSettings', nil, miningSettings)
+    if args and (args[1] == "config" or args[1] == "Config") then
+        config()
+        return
+    end
+    miningLib.permanentFacingPostition = facingPostition()
+
+    --Manuell Distance selection
+    print("How far in Blocks?")
+    local areas = read()
+    areas = tonumber(areas)
+
+    --calc AreaPoints for mining
+    ---@type ScanDataTable
+    local points = {}
+    areas = areas / (miningSettings.miningDiameter)
+    areas = math.floor(areas)
+    table.insert(points, { x = 0, y = 0, z = 0 })
+    for i = 1, areas do
+        --TODO facing! the createpath does not take absolute points!
+        table.insert(points, { x = miningSettings.miningDiameter, y = 0, z = 0 })
+    end
+    miningLib:main(points);
+end
+
+main()
